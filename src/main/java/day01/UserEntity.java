7f29ade5fd5b43e6db887a2432fd2ee992d8a8d4
liuchuangui <200537930@qq.com>
2018-06-06 13:18:37 +0800

    test bean factory

---
+++ src/main/java/day01/UserEntity.java
@@ -0,0 +1,17 @@
+package day01;
+
+/**
+ * Created by Administrator on 2018/6/6.
+ */
+public class UserEntity {
+
+    private String message = "Hi, Spring";
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
